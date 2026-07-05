d33f5f43f5d3af07285261fabaeab51af21ba7af
hanwoolhanwool <hanwoolisland@gmail.com>
2026-07-06 07:40:52 +0900

    chore(player): 스킬 테스트용 런타임 디버그 커맨드 추가
    에디터/런타임에서 스킬 흐름을 점검하기 위한 PlayerDebugCommands 를 추가한다.

---
+++ Assets/Idle Game/Scripts/Features/Player/Presentation/PlayerDebugCommands.cs
@@ -0,0 +1,32 @@
+#if UNITY_EDITOR
+using UnityEngine;
+
+/// <summary>
+/// 에디터 전용 디버그 명령 모음. 인스펙터 ContextMenu로 PlayerRoot의
+/// 디버그 훅을 호출한다. 조립 루트(PlayerRoot)에서 디버그 관심사를 분리하고,
+/// 파일 전체가 #if UNITY_EDITOR로 감싸여 빌드에는 포함되지 않는다.
+/// </summary>
+[RequireComponent(typeof(PlayerRoot))]
+public sealed class PlayerDebugCommands : MonoBehaviour
+{
+    [SerializeField] private PlayerRoot playerRoot;
+    [SerializeField] private float testDamage = 25f;
+    [SerializeField] private int testExp = 120;
+
+    private void Reset()
+    {
+        playerRoot = GetComponent<PlayerRoot>();
+    }
+
+    private PlayerRoot Root => playerRoot != null ? playerRoot : GetComponent<PlayerRoot>();
+
+    [ContextMenu("Apply Test Damage")]
+    private void ApplyTestDamage() => Root.DebugApplyDamage(testDamage);
+
+    [ContextMenu("Apply First Start Buff")]
+    private void ApplyFirstStartBuff() => Root.DebugApplyFirstStartBuff();
+
+    [ContextMenu("Gain Test Exp")]
+    private void GainTestExp() => Root.DebugGainExp(testExp);
+}
+#endif
